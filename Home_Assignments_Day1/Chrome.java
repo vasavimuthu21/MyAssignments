fd8189504f296ecae2de3f4608c48d59330b4f8c
vasavimuthu21 <vasavimuthu21@gmail.com>
2026-04-25 21:46:40 +0530

    Add files via upload
    Home_Assignments_Day1

---
+++ Home_Assignments_Day1/Chrome.java
@@ -0,0 +1,23 @@
+package Home_Assignments_Day1;
+
+public class Chrome {
+    public static void main(String[] args) {
+        float version = 91.0f;
+        String developer = "Google";
+        boolean isBeta = false;
+        int releaseYear = 2008;
+        char shortcutKey ='C';
+
+        System.out.println(version);
+        System.out.println(developer);
+        System.out.println(isBeta);
+        System.out.println(releaseYear);
+        System.out.println(shortcutKey);
+
+        System.out.println(developer+ " " +releaseYear);
+
+        //Join Testleaf via Google
+        System.out.println("Join Testleaf via "+developer);
+    }
+
+}
